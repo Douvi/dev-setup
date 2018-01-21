ec7217ddf81e1f07a1113f00a31b3315c66113d0
Edouard <edouard@RockSpoons-MacBook-Pro-11.local>
2018-01-20 22:07:18 -0200

    fix zsh

--- zsh.sh
+++ zsh.sh
@@ -9,14 +9,7 @@ ABSPATH=$(cd "$(dirname "$0")"; pwd)
 # Keep-alive: update existing `sudo` time stamp until the script has finished.
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-# Check for Homebrew,
-# Install if we don't have it
-if test ! $(which brew); then
-  echo "Installing homebrew..."
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-fi
-
-Make sure we’re using the latest Homebrew.
+#Make sure we’re using the latest Homebrew.
 ruby -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # Add alias
